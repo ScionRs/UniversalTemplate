9b79e4957bf2d3d135fc6985ed3e5befeed5901d
ScionRs <grazs116@gmail.com>
2021-06-15 11:59:45 +0300

    Added members-section

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
             .closest('div.container').find('div.works__items').removeClass('works__items-active')
             .eq($(this).index()).addClass('works__items-active');
     });
+
+    $('.carousel__inner').slick({
+        speed: 500,
+        autoPlay: true,
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
