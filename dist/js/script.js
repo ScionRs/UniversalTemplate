d4cdd13b81d0842b5903549d75d1ad3693aad63a
ScionRs <grazs116@gmail.com>
2021-06-18 14:58:48 +0300

    Added responsive menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    const menu = document.querySelector('.header__menu'),
+        menuItem = document.querySelectorAll('.header__menu-item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger-active');
+        menu.classList.toggle('header__menu-active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger-active');
+            menu.classList.toggle('header__menu-active');
+        })
+    })
+
     $('ul.works__tabs').on('click', 'li:not(.works__tab-active)', function () {
         $(this)
             .addClass('works__tab-active').siblings().removeClass('works__tab-active')
